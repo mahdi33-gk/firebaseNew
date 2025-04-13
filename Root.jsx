26d226e9a042e0746c359a681d19d0f4ecca089b
mehedi <mehedi25122003@gmail.com>
2025-04-13 23:48:27 +0600

    react router fully setup

---
+++ Root.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Root = () => {
+    return (
+        <div>
+            <h1>Heythere!</h1>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Root;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
+import { Children, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from '../Root.jsx';
+import Errorpage from './Errorpage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <Errorpage></Errorpage>
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </StrictMode>,
 )
