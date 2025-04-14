3c3a27e0120d2269fb2ac357148a06f53eeba613
mehedi <mehedi25122003@gmail.com>
2025-04-14 11:59:30 +0600

    login and register added and connect each other

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    return (
-        <div>
-            <h2>login here</h2>
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col">
+        <h1 className="text-3xl font-bold">Log in now!</h1>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <div className="card-body">
+            <form>
+              <fieldset className="fieldset">
+                <label className="fieldset-label">Email</label>
+                <input type="email" className="input" placeholder="Email" />
+                <label className="fieldset-label">Password</label>
+                <input
+                  type="password"
+                  className="input"
+                  placeholder="Password"
+                />
+                <div>
+                  <a className="link link-hover">Forgot password?</a>
+                </div>
+                <button className="btn btn-neutral mt-4">Login</button>
+              </fieldset>
+              <h3 className="mt-3">New to this? <Link to={'/register'} className="ml-2 underline">Register now!</Link></h3>
+            </form>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/Register.jsx
+++ src/Register.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            <h2>register here</h2>
+        <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col">
+        <h1 className="text-3xl font-bold">Register now!</h1>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <div className="card-body">
+            <form>
+              <fieldset className="fieldset">
+                <label className="fieldset-label">Email</label>
+                <input type="email" className="input" placeholder="Email" />
+                <label className="fieldset-label">Password</label>
+                <input
+                  type="password"
+                  className="input"
+                  placeholder="Password"
+                />
+                <button className="btn btn-neutral mt-4">Register</button>
+              </fieldset>
+              <h3 className="mt-3">Already have one? <Link to={'/login'} className="ml-2 underline">login now!</Link></h3>
+            </form>
+          </div>
         </div>
+      </div>
+    </div>
     );
 };
 
