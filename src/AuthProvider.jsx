0756722e3d9425b05037c6d12165a348fe3362f4
mehedi <mehedi25122003@gmail.com>
2025-04-14 17:40:54 +0600

    firebase is best..happy firebasing!

--- src/AuthProvider.jsx
+++ src/AuthProvider.jsx
@@ -1,16 +1,28 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 import auth from './firebase.init';
-import {createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import {createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 
 
 export const AuthContext = createContext(null);
+
 const AuthProvider = ({children}) => {
+    const [user,setUser] = useState(null);
     const creatUser = (email,password) =>{
       return createUserWithEmailAndPassword(auth,email,password)  
     }
     const signInusr =(email,password) => {
         return signInWithEmailAndPassword(auth,email,password)
     }
+    onAuthStateChanged(auth,(currentUser)=>{
+        if(currentUser){
+            console.log(currentUser)
+            setUser(currentUser)
+        }
+        else{
+            console.log('there no one buddy!')
+            setUser(null)
+        }
+    })
     const contextInfo ={
         creatUser,
         signInusr
