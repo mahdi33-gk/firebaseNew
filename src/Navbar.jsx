e99fba7d9f28ae8f3b210957453539986b82c822
mehedi <mehedi25122003@gmail.com>
2025-04-14 11:00:12 +0600

    navlink activation done

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -7,7 +7,11 @@ const Navbar = () => {
     <>
       <li>
         <NavLink to={'/'}>Home</NavLink>
+      </li>
+      <li>
         <NavLink to={'/login'}>Login</NavLink>
+      </li>
+      <li>
         <NavLink to={'/register'}>Register</NavLink>
       </li>
       
@@ -38,18 +42,15 @@ const Navbar = () => {
           <ul
             tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-          ></ul>
+          >
+            {links}
+          </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">firebaseNew</a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
+          {links}
         </ul>
       </div>
       <div className="navbar-end">
